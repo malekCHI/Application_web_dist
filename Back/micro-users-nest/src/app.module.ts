57a7b7c9f88e0b28e0446cc0e299e0a9cad2f721
ahmedmarzouk1710 <amarzouk880@gmail.com>
2023-10-20 19:16:46 +0100

    mongodb

--- Back/micro-users-nest/src/app.module.ts
+++ Back/micro-users-nest/src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './users/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/userdb'),
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/userdb?directConnection=true',
+    ),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     AuthModule,
     UsersModule,
   ],
