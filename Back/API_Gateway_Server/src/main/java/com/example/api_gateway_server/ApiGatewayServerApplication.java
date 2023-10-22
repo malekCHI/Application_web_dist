3fa38010e8702e80baa635e67f102cb1be710ba6
MaryemBenMohamed <72559644+MaryemBenMohamed@users.noreply.github.com>
2023-10-22 20:17:30 +0100

    eureka+gateway

---
+++ Back/API_Gateway_Server/src/main/java/com/example/api_gateway_server/ApiGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.api_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ Back/API_Gateway_Server/src/test/java/com/example/api_gateway_server/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
