0ba73c13138318bb780660d235dc89b2a7848b06
ahmedmarzouk1710 <amarzouk880@gmail.com>
2023-10-24 17:52:49 +0100

    front+back departement event

--- Front/src/app/apps/departement/departement.component.ts
+++ Front/src/app/apps/departement/departement.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceblogService } from '../blog/blog-service.service';
 import { Blog } from '../blog/blog-type';
+import { DepartementService } from 'src/app/services/departement.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Departement } from 'src/app/model/departement';
 
 @Component({
   selector: 'app-departement',
@@ -9,13 +12,52 @@ import { Blog } from '../blog/blog-type';
   styleUrls: ['./departement.component.css']
 })
 export class DepartementComponent implements OnInit {
-  id: any;
-  blogDetail: Blog | null = null;
+  showDepartmentForm = false;
+  departements:any
+  isActive: boolean = false;
+  departement!:Departement;
+  
 
-  constructor(activatedRouter: ActivatedRoute, public service: ServiceblogService, public router: Router) { }
+  constructor(private depserv: DepartementService) {
+    this.departement ={
+      id: 0,
+      name: "",
+      active: false,
+      stats: 0,
+      head: "",
+      description : ""
+    }
+   }
 
   ngOnInit(): void {
-    this.blogDetail = this.service.Blogs.filter(x => x.id === +this.id)[0]
+    this.getDeps();
+    this.showDepartmentForm = false;
   }
+  
+
+deleteDep(idEvent : any){
+  this.depserv.deleteDep(idEvent).subscribe(() => this.getDeps())
+  location.reload();
+  }
+  public getDeps():void{
+    this.depserv.getDeps().subscribe(  
+      (response : Departement[]) =>{
+        this.departements=response
+        console.log(response)
+      },(error :HttpErrorResponse)=>{
+        alert(error.message);
+      });
+
+}
+toggleDepartmentForm() {
+  this.showDepartmentForm = !this.showDepartmentForm;
+}
+addDepartment(dep:any) {
+  this.depserv.addDep(dep).subscribe(() => {
+    this.getDeps();
+    this.showDepartmentForm = false;
+  });
+  
+}
 
 }
